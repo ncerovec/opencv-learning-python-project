f64a1d47ce54a9bde311563f5c8c95f0902b1c8b
Bruno Matic <bruno.matic.zg@gmail.com>
2016-12-29 23:31:20 +0000

    Cleaning up the code and repo

--- deep-learning/custom_3_86_final/cifar10_custom.py
+++ deep-learning/custom_3_86_final/cifar10_custom.py
@@ -5,7 +5,7 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import Flatten
 from keras.constraints import maxnorm
-from keras.optimizers import SGD
+from keras.utils.visualize_util import plot
 from keras.layers.convolutional import Convolution2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
@@ -14,22 +14,27 @@ from keras import backend as K
 
 K.set_image_dim_ordering('th')
 
+# randomize just to be sure
 seed = 7
 numpy.random.seed(seed)
 
+# load CIFAR-10 dataset and it lables
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
+# convert pixels to floats and normalize to 0.0-1.0
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
-
 X_train /= 255
 X_test /= 255
 
+# one-of-K scheme for label encoding
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 
+# get number of classed
 num_classes = y_test.shape[1]
 
+# create a new image generator with necessary transformations
 train_datagen = ImageDataGenerator(
     shear_range=0.2,
     zoom_range=0.1,
@@ -38,44 +43,59 @@ train_datagen = ImageDataGenerator(
     channel_shift_range=0.2,
     horizontal_flip=True)
 
-
+# create a flow from the data generator for training
 train_generator = train_datagen.flow(X_train, y_train, batch_size=64)
 
+# instantiate a model
 model = Sequential()
-model.add(
-    Convolution2D(32, 3, 3, input_shape=(3, 32, 32), border_mode='same', activation='relu'))
+# first build unit - 32
+model.add(Convolution2D(32, 3, 3, input_shape=(3, 32, 32), border_mode='same', activation='relu'))
 model.add(Dropout(0.2))
 model.add(Convolution2D(32, 3, 3, activation='relu', border_mode='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+# second build unit - 64
 model.add(Convolution2D(64, 3, 3, activation='relu', border_mode='same'))
 model.add(Dropout(0.2))
 model.add(Convolution2D(64, 3, 3, activation='relu', border_mode='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+# third build unit - 128
 model.add(Convolution2D(128, 3, 3, activation='relu', border_mode='same'))
 model.add(Dropout(0.2))
 model.add(Convolution2D(128, 3, 3, activation='relu', border_mode='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+# fourth build unit - 256
 model.add(Convolution2D(256, 3, 3, activation='relu', border_mode='same'))
 model.add(Dropout(0.2))
 model.add(Convolution2D(256, 3, 3, activation='relu', border_mode='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+# full connected layer
 model.add(Flatten())
 model.add(Dropout(0.2))
+# weight normalization with max norm
 model.add(Dense(1024, activation='relu', W_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
+# once again weight normalization
 model.add(Dense(512, activation='relu', W_constraint=maxnorm(3)))
 model.add(Dropout(0.2))
+# output to classes with softmax
 model.add(Dense(num_classes, activation='softmax'))
 
-epochs = 100
-# learning_rate = 0.01
-# decay = learning_rate / epochs
-# sgd = SGD(lr=learning_rate, momentum=0.9, decay=decay, nesterov=False)
+# store a picture for us to see
+print("Model visualisation saved to custom.png")
+plot(model, to_file='custom.png', show_shapes=True, show_layer_names=True)
+
+# compile the model with loss function for categories, use Adadelta and show us accuracy for each epoch
 model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+# print us a model summary
 print(model.summary())
 
+# just to be sure
 numpy.random.seed(seed)
 
+# train the model for 100 epochs using the train generator flow and training set size
+# validate everything on untouched data and show us only final progress
+epochs = 100
 model.fit_generator(
     train_generator,
     samples_per_epoch=X_train.shape[0],
@@ -83,5 +103,7 @@ model.fit_generator(
     validation_data=(X_test, y_test),
     verbose=2)
 
+# not needed - but nice to see
+# check for the final score and print it
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1] * 100))
--- deep-learning/vgg16_10_77_final/cifar10_vgg16.py
+++ deep-learning/vgg16_10_77_final/cifar10_vgg16.py
@@ -1,25 +1,57 @@
-from __future__ import print_function
-import os
 import h5py
 import numpy
 from keras.utils import np_utils
 from keras.constraints import maxnorm
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import SGD
+from keras.utils.visualize_util import plot
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Dropout, Flatten, Dense
 from keras.datasets import cifar10
 from keras import backend as K
 
 K.set_image_dim_ordering('th')
 
-# path to the model weights files.
-weights_path = 'vgg16_weights.h5'
+# randomize just to be sure
+seed = 7
+numpy.random.seed(seed)
+
+# load CIFAR-10 dataset and it lables
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+# convert pixels to floats and normalize to 0.0-1.0
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+# one-of-K scheme for label encoding
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+# get number of classed
+num_classes = y_test.shape[1]
+
+# create a new image generator with necessary transformations
+train_datagen = ImageDataGenerator(
+    shear_range=0.2,
+    zoom_range=0.1,
+    height_shift_range=0.1,
+    width_shift_range=0.1,
+    channel_shift_range=0.2,
+    horizontal_flip=True)
+
+# create a flow from the data generator for training
+train_generator = train_datagen.flow(X_train, y_train, batch_size=64)
+
+
+# path to the VGG16 weights files.
+weights_path = '../vgg16_weights.h5'
+
 # dimensions of our images.
 img_width, img_height = 32, 32
 
-# build the VGG16 network
+# build the VGG16 network since we can't import it
 model = Sequential()
 model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
 model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
@@ -58,10 +90,6 @@ model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
 model.add(MaxPooling2D((2, 2), strides=(2, 2)))
 
 # load the weights of the VGG16 networks
-# (trained on ImageNet, won the ILSVRC competition in 2014)
-# note: when there is a complete match between your model definition
-# and your weight savefile, you can simply call model.load_weights(filename)
-assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
 f = h5py.File(weights_path)
 for k in range(f.attrs['nb_layers']):
     if k >= len(model.layers):
@@ -73,50 +101,42 @@ for k in range(f.attrs['nb_layers']):
 f.close()
 print('Model loaded.')
 
-# build a classifier model to put on top of the convolutional model
+# build a classifier model (a fully connected layer) to put on top of the VGG16 model
+# use weight normalization with aggressive dropout 40%
 top_model = Sequential()
+# use input shape of last convolution block from VGG16
 top_model.add(Flatten(input_shape=model.output_shape[1:]))
 top_model.add(Dense(1024, activation='relu', W_constraint=maxnorm(3)))
 top_model.add(Dropout(0.4))
 top_model.add(Dense(512, activation='relu', W_constraint=maxnorm(3)))
 top_model.add(Dropout(0.4))
-top_model.add(Dense(10, activation='softmax'))
-
+top_model.add(Dense(num_classes, activation='softmax'))
+# stick it to the bottom of VGG16
 model.add(top_model)
 
+
+# save us pictures of both
+print("FC layers visualisation saved to fc.png")
+plot(top_model, to_file='fc.png', show_shapes=True, show_layer_names=True)
+
+print("Model visualisation saved to vgg16.png")
+plot(model, to_file='vgg16.png', show_shapes=True, show_layer_names=True)
+
+
+# freeze all layers except last convolution block and our fully connected layer
 for layer in model.layers[:25]:
     layer.trainable = False
 
-epochs = 100
+# compile the model with loss function for categories, use Adadelta and show us accuracy for each epoch
 model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
-
-seed = 7
-numpy.random.seed(seed)
-
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-
-X_train /= 255
-X_test /= 255
-
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
+# print us a summary of model
+print(model.summary())
 
 
-train_datagen = ImageDataGenerator(
-    shear_range=0.2,
-    zoom_range=0.1,
-    height_shift_range=0.1,
-    width_shift_range=0.1,
-    channel_shift_range=0.2,
-    horizontal_flip=True)
-
-train_generator = train_datagen.flow(X_train, y_train, batch_size=64)
-
-# fine-tune the model
+# train the model for 100 epochs using the train generator flow and training set size
+# validate everything on untouched data and show us only final progress
+epochs = 100
 model.fit_generator(
     train_generator,
     samples_per_epoch=X_train.shape[0],
@@ -124,5 +144,7 @@ model.fit_generator(
     validation_data=(X_test, y_test),
     verbose=2)
 
+# not needed - but nice to see
+# check for the final score and print it
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1] * 100))
